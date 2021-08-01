dab422638ff3ad62bde15e0a3d04a048b52265dc
streetsummit <streetsummit@mail.ru>
2021-08-02 00:22:11 +0300

    Добавляет структуру данных Destination

--- src/mocks/mock-event.js
+++ src/mocks/mock-event.js
@@ -1,12 +1,35 @@
-import { getRandomInteger, getRandomArrayElement } from '../utils.js';
+import { getRandomInteger, getRandomArrayElement, getRandomLengthArray } from '../utils.js';
 
 const TYPES = ['taxi', 'bus', 'train', 'ship', 'drive', 'flight', 'check-in', 'sightseeing', 'restaurant'];
+const CITIES = ['Amsterdam', 'Chamonix', 'Geneva', 'Paris', 'London'];
+
+const SENTENCES = [
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  'Cras aliquet varius magna, non porta ligula feugiat eget.',
+  'Fusce tristique felis at fermentum pharetra.',
+  'Aliquam id orci ut lectus varius viverra.',
+  'Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante.',
+  'Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum.',
+  'Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui.',
+  'Sed sed nisi sed augue convallis suscipit in sed felis.',
+  'Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus.',
+  'In rutrum ac purus sit amet tempus.',
+];
+
+const generateDestination = () => ({
+  description: getRandomLengthArray(SENTENCES, 1, 5),
+  name: getRandomArrayElement(CITIES),
+  pictures: new Array(getRandomInteger(1, 5)).fill(null).map(() => ({
+    src: `http://picsum.photos/300/200?r=${Math.random()}`,
+    description: getRandomArrayElement(SENTENCES),
+  })),
+});
 
 const generateEvent = () => ({
   dateFrom: '2019-03-19T11:20',
   dateTo: '2019-03-19T13:00',
   type: getRandomArrayElement(TYPES),
-  destination: 'Chamonix',
+  destination: generateDestination(),
   price: getRandomInteger(0, 250),
   offers: [],
   isFavorite: Boolean(getRandomInteger(0, 1)),
--- src/utils.js
+++ src/utils.js
@@ -7,7 +7,20 @@ const getRandomInteger = (min, max) => {
 
 const getRandomArrayElement = (array) => array[getRandomInteger(0, array.length - 1)];
 
+const getRandomLengthArray = (array, min = 0, max = array.length) => {
+  const dataArray = array.slice();
+  const randomLengthArray = [];
+  const randomLength = getRandomInteger(min, max);
+  for (let i = 0; i < randomLength; i++) {
+    const position = getRandomInteger(0, dataArray.length - 1);
+    const element = dataArray.splice(position, 1)[0];
+    randomLengthArray.push(element);
+  }
+  return randomLengthArray;
+};
+
 export {
   getRandomInteger,
-  getRandomArrayElement
+  getRandomArrayElement,
+  getRandomLengthArray
 };
