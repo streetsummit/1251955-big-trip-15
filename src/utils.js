eeb20393667fa89cb3326b826a76a81ff182fd65
streetsummit <streetsummit@mail.ru>
2021-08-03 00:33:00 +0300

    Учит функцию выдавать случайное отрицательное число

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,6 @@
 const getRandomInteger = (min, max) => {
-  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
-  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
+  const lower = Math.ceil(Math.min(min, max));
+  const upper = Math.floor(Math.max(min, max));
   const result = Math.random() * (upper - lower + 1) + lower;
   return Math.floor(result);
 };
