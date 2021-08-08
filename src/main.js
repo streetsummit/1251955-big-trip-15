8a4399a0b64745c2d39f5be735a9d0ec8483552f
streetsummit <streetsummit@mail.ru>
2021-08-09 02:22:52 +0300

    Опишет компонент сортировки как класс

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { renderTemplate, renderElement, RenderPosition } from './utils.js';
 import MenuView from './view/menu.js';
 import { createTripInfoTemplate } from './view/trip-info.js';
 import { createTripFilterTemplate } from './view/filter.js';
-import { createTripSortTemplate } from './view/trip-sort.js';
+import TripSortView from './view/trip-sort.js';
 import { createEventListTemplate } from './view/event-list.js';
 import { createEventTemplate } from './view/event.js';
 import { createEventEditTemplate } from './view/event-edit.js';
@@ -21,10 +21,9 @@ const events = new Array(EVENTS_COUNT).fill(null).map(generateEvent);
 const eventElements = events.slice(1).map(createEventTemplate);
 
 renderElement(siteMenuElement, new MenuView().getElement(), RenderPosition.BEFOREEND);
-
 renderTemplate(tripMainElement, createTripInfoTemplate(), 'afterbegin');
 renderTemplate(tripFilterElement, createTripFilterTemplate(), 'beforeend');
-renderTemplate(tripEventsElement, createTripSortTemplate(), 'beforeend');
+renderElement(tripEventsElement, new TripSortView().getElement(), RenderPosition.BEFOREEND);
 renderTemplate(tripEventsElement, createEventListTemplate(createEventEditTemplate(events[0]), ...eventElements), 'beforeend');
 
 console.log(events);
