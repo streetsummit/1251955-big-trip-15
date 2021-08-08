f17b3d650f8ae261f400f0e22b7061ca54c4dee0
streetsummit <streetsummit@mail.ru>
2021-08-09 01:50:08 +0300

    small fix

--- src/utils.js
+++ src/utils.js
@@ -24,7 +24,7 @@ export const renderTemplate = (container, template, place) => {
 
 export const createElement = (template) => {
   const newElement = document.createElement('div');
-  newElement.innerHTML(template);
+  newElement.innerHTML = template;
   return newElement.firstChild;
 };
 
