f2ab36b261db78d83c8bac4c773ab3188fd11c6a
streetsummit <streetsummit@mail.ru>
2021-08-08 20:26:21 +0300

    Рефакторит рендер-метод

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import { renderTemplate } from './utils.js';
 import { createMenuTemplate } from './view/menu.js';
 import { createTripInfoTemplate } from './view/trip-info.js';
 import { createTripFilterTemplate } from './view/filter.js';
@@ -19,14 +20,10 @@ const tripEventsElement = siteMainElement.querySelector('.trip-events');
 const events = new Array(EVENTS_COUNT).fill(null).map(generateEvent);
 const eventElements = events.slice(1).map(createEventTemplate);
 
-const render = (container, template, place) => {
-  container.insertAdjacentHTML(place, template);
-};
-
-render(siteMenuElement, createMenuTemplate(), 'beforeend');
-render(tripMainElement, createTripInfoTemplate(), 'afterbegin');
-render(tripFilterElement, createTripFilterTemplate(), 'beforeend');
-render(tripEventsElement, createTripSortTemplate(), 'beforeend');
-render(tripEventsElement, createEventListTemplate(createEventEditTemplate(events[0]), ...eventElements), 'beforeend');
+renderTemplate(siteMenuElement, createMenuTemplate(), 'beforeend');
+renderTemplate(tripMainElement, createTripInfoTemplate(), 'afterbegin');
+renderTemplate(tripFilterElement, createTripFilterTemplate(), 'beforeend');
+renderTemplate(tripEventsElement, createTripSortTemplate(), 'beforeend');
+renderTemplate(tripEventsElement, createEventListTemplate(createEventEditTemplate(events[0]), ...eventElements), 'beforeend');
 
 console.log(events);
--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,33 @@
 const TYPES = ['taxi', 'bus', 'train', 'ship', 'drive', 'flight', 'check-in', 'sightseeing', 'restaurant'];
 const SYMBOLS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
+export const RenderPosition = {
+  AFTERBEGIN: 'afterbegin',
+  BEFOREEND: 'beforeend',
+};
+
+export const renderElement = (container, element, place) => {
+  switch (place) {
+    case RenderPosition.AFTERBEGIN:
+      container.prepend(element);
+      break;
+    case RenderPosition.BEFOREEND:
+      container.append(element);
+      break;
+  }
+};
+
+
+export const renderTemplate = (container, template, place) => {
+  container.insertAdjacentHTML(place, template);
+};
+
+export const createElement = (template) => {
+  const newElement = document.createElement('div');
+  newElement.innerHTML(template);
+  return newElement.firstChild;
+};
+
 const getRandomInteger = (min, max) => {
   const lower = Math.ceil(Math.min(min, max));
   const upper = Math.floor(Math.max(min, max));
