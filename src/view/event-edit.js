69e547fcba7f960232af05e1fc5bf0931a3a79ae
streetsummit <streetsummit@mail.ru>
2021-08-26 16:19:23 +0300

    Убирает лишнюю ветку else

--- src/view/event-edit.js
+++ src/view/event-edit.js
@@ -75,8 +75,9 @@ const createOffersTemplate = (availableOffers, currentOffers, hasAvailableOffers
         ${createOffersListTemplate(availableOffers, currentOffers)}
       </div>
     </section>`;
-  } else {
-    return '';
+  }
+  return '';
+};
   }
 };
 
