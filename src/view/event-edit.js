b2fa8c37d471e51b6b517a796bc7f0dade20dc4e
streetsummit <streetsummit@mail.ru>
2021-08-27 01:06:52 +0300

    small fix. Подпись кнопки закрытия формы

--- src/view/event-edit.js
+++ src/view/event-edit.js
@@ -171,7 +171,7 @@ const createEventEditTemplate = (data) => {
         <button class="event__save-btn  btn  btn--blue" type="submit">Save</button>
         <button class="event__reset-btn" type="reset">Delete</button>
         <button class="event__rollup-btn" type="button">
-          <span class="visually-hidden">Open event</span>
+          <span class="visually-hidden">Close event</span>
         </button>
       </header>
       <section class="event__details">
