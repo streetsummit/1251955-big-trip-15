97133adb5fde137430395999b55c08a5fefe8fbf
streetsummit <streetsummit@mail.ru>
2021-09-02 17:23:45 +0300

    Рефакторит добавление класса избранного

--- src/view/event.js
+++ src/view/event.js
@@ -39,8 +39,8 @@ const createEventTemplate = (event) => {
   const durationTemplate = createEventDurationTemplate(dateFrom, dateTo);
   const offersTemplate = createEventOfferListTemplate(offers);
   const favoriteClassName = isFavorite
-    ? 'event__favorite-btn  event__favorite-btn--active'
-    : 'event__favorite-btn';
+    ? 'event__favorite-btn--active'
+    : '';
 
   return `<li class="trip-events__item">
     <div class="event">
@@ -62,7 +62,7 @@ const createEventTemplate = (event) => {
       </p>
       <h4 class="visually-hidden">Offers:</h4>
       ${offersTemplate}
-      <button class="${favoriteClassName}" type="button">
+      <button class="event__favorite-btn ${favoriteClassName}" type="button">
         <span class="visually-hidden">Add to favorite</span>
         <svg class="event__favorite-icon" width="28" height="28" viewBox="0 0 28 28">
           <path d="M14 21l-8.22899 4.3262 1.57159-9.1631L.685209 9.67376 9.8855 8.33688 14 0l4.1145 8.33688 9.2003 1.33688-6.6574 6.48934 1.5716 9.1631L14 21z"/>
