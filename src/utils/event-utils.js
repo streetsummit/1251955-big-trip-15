94a736e45872b5ce848e97c66cdd9f1bb5c5c4ab
streetsummit <streetsummit@mail.ru>
2021-09-29 16:22:57 +0300

    Исправляет добавление ведущего нуля

--- src/utils/event-utils.js
+++ src/utils/event-utils.js
@@ -15,21 +15,16 @@ export const getFullEventPrice = (event) => event.offers.reduce((sum, current) =
 export const getTripPrice = (events) => events.reduce(((sum, event) => sum + getFullEventPrice(event)), 0);
 
 export const formatDuration = (duration) => {
+  const days = Math.floor(duration / MIN_IN_DAY);
+  const hours = Math.floor((duration - (days * MIN_IN_DAY)) / MIN_IN_HOUR);
+  const minutes = duration - (days * MIN_IN_DAY) - (hours * MIN_IN_HOUR);
 
-  let days = Math.floor(duration / MIN_IN_DAY);
-  let hours = Math.floor((duration - (days * MIN_IN_DAY)) / MIN_IN_HOUR);
-  let minutes = duration - (days * MIN_IN_DAY) - (hours * MIN_IN_HOUR);
-
-  days = days < 10 ? `0${days}` : days;
-  hours = hours < 10 ? `0${hours}` : hours;
-  minutes = minutes < 10 ? `0${minutes}` : minutes;
-
-  if (days > 0) {
-    return `${days}D ${hours}H ${minutes}M`;
-  } else if (hours > 0) {
-    return `${hours}H ${minutes}M`;
+  if (days) {
+    return `${days.toString().padStart(2, '0')}D ${hours.toString().padStart(2, '0')}H ${minutes.toString().padStart(2, '0')}M`;
+  } else if (hours) {
+    return `${hours.toString().padStart(2, '0')}H ${minutes.toString().padStart(2, '0')}M`;
   }
-  return `${minutes}M`;
+  return `${minutes.toString().padStart(2, '0')}M`;
 };
 
 export const getCountByType = (events, type) => events.filter((event) => event.type === type).length;
