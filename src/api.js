aa88a0a3ddeaeab0547ba8e33cca44885a4ea7d9
streetsummit <streetsummit@mail.ru>
2021-10-29 16:53:11 +0300

    Создаёт класс для работы с Api

---
+++ src/api.js
@@ -0,0 +1,47 @@
+const Method = {
+  GET: 'GET',
+  PUT: 'PUT',
+};
+
+export default class Api {
+  constructor(endPoint, authorization) {
+    this._endPoint = endPoint;
+    this._authorization = authorization;
+  }
+
+  getEvents() {
+    return this._load({url: 'points'})
+      .then(Api.toJSON);
+  }
+
+  _load({
+    url,
+    method = Method.GET,
+    body = null,
+    headers = new Headers(),
+  }) {
+    headers.append('Authorization', this._authorization);
+
+    return fetch(
+      `${this._endPoint}/${url}`,
+      {method, body, headers},
+    )
+      .then(Api.checkStatus)
+      .catch(Api.catchError);
+  }
+
+  static checkStatus(response) {
+    if (!response.ok) {
+      throw new Error(`${response.status}: ${response.statusText}`);
+    }
+    return response;
+  }
+
+  static catchError(err) {
+    throw err;
+  }
+
+  static toJSON(response) {
+    return response.json();
+  }
+}
